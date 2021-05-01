b9887464dc215c90ea9c5de4735b83564742e452
Jerry Ng <jerryng500@hotmail.com>
2021-04-30 20:13:03 -0400

    Updated keyboard_process to not require root access

--- corona_ws/src/user_input/src/keyboard_process.py
+++ corona_ws/src/user_input/src/keyboard_process.py
@@ -5,7 +5,10 @@ import rospy
 import numpy as np
 import keyboard
 from numpy.linalg import inv
-
+import sys
+import termios
+import os
+import tty
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import Point, Pose, Twist, Vector3, Quaternion
 from std_msgs.msg import ColorRGBA, Float32, Bool, Int32
@@ -25,7 +28,7 @@ class keyboard_obj(object):
 		self.cmd_msg = JoyCmd()
 		self.conn_msg = Bool()
 		self.conn_msg.data = True
-
+		self.mult = 1.0/3.0
 		while not rospy.is_shutdown():
 			self.cmd_msg = self.detectCmds()
 			# Publish at a frequency of 60 Hz
@@ -33,38 +36,55 @@ class keyboard_obj(object):
 			self.joycmdPub.publish(self.cmd_msg)
 			r.sleep()
 
+	def getch(self):
+		fd = sys.stdin.fileno()
+		old_settings = termios.tcgetattr(fd)
+		try:
+			tty.setraw(sys.stdin.fileno())
+			ch = sys.stdin.readline(1)
+	 
+		finally:
+			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+		return ch
+
 	def detectCmds(self):
+		char = self.getch()
+		print(char)
 		msg = JoyCmd()
-		mult = 1.0/3.0
 		#Detect forward and backward keys assigned to 'w' and 's'
-		if keyboard.is_pressed('w'):
+		if 'w' in char:
 			msg.axis1 =  1.0
-		if keyboard.is_pressed('s'):
+		elif 's' in char:
 			msg.axis1 = msg.axis1 - 1.0
 
 		#Detect left and right keys 'a' and 'd'
-		if keyboard.is_pressed('a'):
+		elif 'a' in char:
 			msg.axis2 =  1.0
-		if keyboard.is_pressed('d'):
+		elif 'd' in char:
 			msg.axis2 = msg.axis2 - 1.0
 
 		#Detect rotation keys 'q' for counterclockwise, 'e' for clockwise
 
-		if keyboard.is_pressed('q'):
+		elif 'q' in char:
 			msg.axis3 =  1.0
-		if keyboard.is_pressed('e'):
+		elif 'e' in char:
 			msg.axis3 = msg.axis3 - 1.0
 
 		#Detect keys for multipliers
+		elif 'h' in char:
+			self.mult = 1.0/3.0
+		elif 'j' in char:
+			self.mult = 2.0/3.0
+		elif 'k' in char:
+			self.mult = 1.0
 
-		if keyboard.is_pressed('alt'):
-			mult = 2.0/3.0
-		if keyboard.is_pressed('shift'):
-			mult = 1.0
+		#Dummy character that also works to stop the vehicle
+		elif '0' in char:
+			pass
 
-		msg.axis1 = msg.axis1*mult
-		msg.axis2 = msg.axis2*mult
-		msg.axis3 = msg.axis3*mult
+		msg.axis1 = msg.axis1*self.mult
+		msg.axis2 = msg.axis2*self.mult
+		msg.axis3 = msg.axis3*self.mult
 		return msg
 
 
